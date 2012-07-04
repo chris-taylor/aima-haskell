d062cb1bcb51858599276581bc137b94aa7b85d8
Chris Taylor <crntaylor@gmail.com>
2012-07-04 11:13:18 +0100

    Added documentation

--- src/AI/Search.hs
+++ src/AI/Search.hs
@@ -258,33 +258,50 @@ hillClimbingSearch prob = go (root prob)
 -- Graphs and Graph Problems --
 -------------------------------
 
+-- |Data structure to hold a graph (edge weights correspond to the distance
+--  between nodes) and a map of graph nodes to locations.
 data GraphMap a = G
     { getGraph     :: Graph a Cost
-    , getLocations :: Map a Point } deriving (Show)
+    , getLocations :: Map a Location } deriving (Show)
 
-type Point = (Double,Double)
+-- |Type synonym for a pair of doubles, representing a location in cartesian
+--  coordinates.
+type Location = (Double,Double)
 
-mkGraphMap :: (Ord a) => [(a,[(a,Cost)])] -> [(a,Point)] -> GraphMap a
+-- |Creates a GraphMap from the graph's adjacency list representation and a list
+--  of (node, location) pairs. This function creates undirected graphs, so you
+--  don't need to include reverse links in the adjacency list (though you can
+--  if you like).
+mkGraphMap :: (Ord a) => [(a,[(a,Cost)])] -> [(a,Location)] -> GraphMap a
 mkGraphMap conn loc = G (G.toUndirectedGraph conn) (M.fromList loc)
 
+-- |Get the neighbours of a node from a GraphMap.
 getNeighbors :: Ord a => a -> GraphMap a -> [(a,Cost)]
 getNeighbors a (G g _) = G.getNeighbours a g
 
-getLocation :: Ord a => a -> GraphMap a -> Point
+-- |Get the location of a node from a GraphMap.
+getLocation :: Ord a => a -> GraphMap a -> Location
 getLocation a (G _ l) = case M.lookup a l of
     Nothing -> error "Vertex not found in graph -- GETLOCATION"
     Just pt -> pt
 
+-- |The cost associated with moving between two nodes in a GraphMap. If the
+--  nodes are not connected by an edge, then the cost is returned as infinity.
 costFromTo :: Ord a => GraphMap a -> a -> a -> Cost
 costFromTo graph a b = case lookup b (getNeighbors a graph) of
     Nothing -> 1/0
     Just c  -> c
 
+-- |Data structure to hold a graph problem (represented as a GraphMap together
+--  with an initial and final node).
 data GraphProblem s a = GP
     { graphGP :: GraphMap s
     , initGP :: s
     , goalGP :: s } deriving (Show)
 
+-- |GraphProblems are an instance of Problem. The heuristic function measures
+--  the Euclidean (straight-line) distance between two nodes. It is assumed that
+--  this is less than or equal to the cost of moving along edges.
 instance Ord s => Problem GraphProblem s s where
     initial = initGP
     goal = goalGP
@@ -295,9 +312,11 @@ instance Ord s => Problem GraphProblem s s where
             x = getLocation (state n) g
             y = getLocation goal g
 
-euclideanDist :: Point -> Point -> Double
+-- |Measures the Euclidean (straight-line) distance between two locations.
+euclideanDist :: Location -> Location -> Double
 euclideanDist (x,y) (x',y') = sqrt $ (x-x')^2 + (y-y')^2
 
+-- |The Romania graph from AIMA.
 romania :: GraphMap String
 romania = mkGraphMap
 
@@ -321,6 +340,7 @@ romania = mkGraphMap
     , ("O",(131,571)), ("P",(320,368)), ("R",(233,410)), ("S",(207,457))
     , ("T",( 94,410)), ("U",(456,350)), ("V",(509,444)), ("Z",(108,531)) ]
 
+-- |The Australis graph from AIMA.
 australia :: GraphMap String
 australia = mkGraphMap
 
@@ -332,8 +352,8 @@ australia = mkGraphMap
     [ ("WA",(120,24)), ("NT" ,(135,20)), ("SA",(135,30)),
       ("Q" ,(145,20)), ("NSW",(145,32)), ("T" ,(145,42)), ("V",(145,37))]
 
+-- |Three example graph problems from AIMA.
 gp1, gp2, gp3  :: GraphProblem String String
-
 gp1 = GP { graphGP = australia, initGP = "Q", goalGP = "WA" }
 gp2 = GP { graphGP = romania, initGP = "A", goalGP = "B" }
 gp3 = GP { graphGP = romania, initGP = "O", goalGP = "N" }
@@ -342,6 +362,8 @@ gp3 = GP { graphGP = romania, initGP = "O", goalGP = "N" }
 -- N Queens Problem --
 ----------------------
 
+-- |Data structure to define an N-Queens problem (the problem is defined by
+--  the size of the board).
 data NQueens s a = NQ { sizeNQ :: Int } deriving (Show)
 
 -- |Update the state of the N-Queens board by playing a queen at (i,n).
@@ -362,6 +384,7 @@ conflicted state row col = or $ map f (enumerate state)
             then False
             else conflict row col r c
 
+-- |N-Queens is an instance of Problem. 
 instance Problem NQueens [Maybe Int] (Int,Int) where
     initial (NQ n) = replicate n Nothing
 
@@ -380,21 +403,25 @@ instance Problem NQueens [Maybe Int] (Int,Int) where
                 f (c,Nothing) = False
                 f (c,Just r)  = conflicted s r c
 
-p :: NQueens [Maybe Int] (Int,Int)
-p = NQ 8
+-- |An example N-Queens problem on an 8x8 grid.
+nQueens :: NQueens [Maybe Int] (Int,Int)
+nQueens = NQ 8
 
 -----------------------
 -- Compare Searchers --
 -----------------------
 
 -- |Wrapper for a problem that keeps statistics on how many times nodes were
---  expanded in the course of a search. 
+--  expanded in the course of a search. We track the number of times 'goalCheck'
+--  was called, the number of times 'successor' was called, and the total number
+--  of states expanded.
 data ProblemIO p s a = PIO
     { problemIO     :: p s a
     , numGoalChecks :: IORef Int
     , numSuccs      :: IORef Int
     , numStates     :: IORef Int }
 
+-- |Construct a new ProblemIO, with all counters initialized to zero.
 mkProblemIO :: p s a -> IO (ProblemIO p s a)
 mkProblemIO p = do
     i <- newIORef 0
@@ -402,6 +429,8 @@ mkProblemIO p = do
     k <- newIORef 0
     return (PIO p i j k)
 
+-- |Make ProblemIO into an instance of Problem. It uses the same implementation
+--  as the problem it wraps, except that whenever 'goalTest' or 's'
 instance (Problem p s a, Eq s, Show s) => Problem (ProblemIO p) s a where
     initial (PIO p _ _ _) = initial p
 
@@ -419,6 +448,9 @@ instance (Problem p s a, Eq s, Show s) => Problem (ProblemIO p) s a where
 
     heuristic (PIO p _ _ _) = heuristic p
 
+-- |Given a problem and a search algorithm, run the searcher on the problem
+--  and return the solution found, together with statistics about how many
+--  nodes were expanded in the course of finding the solution.
 testSearcher :: p s a -> (ProblemIO p s a -> t) -> IO (t,Int,Int,Int)
 testSearcher prob searcher = do
     p@(PIO _ numGoalChecks numSuccs numStates) <- mkProblemIO prob
@@ -428,14 +460,19 @@ testSearcher prob searcher = do
         k <- readIORef numStates
         return (result, i, j, k)
 
+-- |Test multiple searchers on the same problem, and return a list of results
+--  and statistics.
 testSearchers :: [ProblemIO p s a -> t] -> p s a -> IO [(t,Int,Int,Int)]
 testSearchers searchers prob = testSearcher prob `mapM` searchers
 
-compareSearchers :: (Show t) => [ProblemIO p s a -> t]
-                 -> [p s a]
-                 -> [String]
-                 -> [String]
-                 -> IO [[(t,Int,Int,Int)]] 
+-- |Given a list of problems and a list of searchers, run 'testSearcher'
+--  pairwise and print out a table showing the performance of each algorithm.
+compareSearchers :: (Show t) =>
+                    [ProblemIO p s a -> t]  -- ^ List of search algorithms
+                 -> [p s a]                 -- ^ List of problems
+                 -> [String]                -- ^ Names of the search algorithms
+                 -> [String]                -- ^ Names of the problems
+                 -> IO [[(t,Int,Int,Int)]]  
 compareSearchers searchers probs header rownames = do
     results <- testSearchers searchers `mapM` probs
     printTable 20 (map (map f) results) header rownames
@@ -443,10 +480,15 @@ compareSearchers searchers probs header rownames = do
     where
         f (x,i,j,k) = (i,j,k)
 
+-- |Given a problem and a list of searchers, run each search algorithm over the
+--  problem, and print out a table showing the performance of each searcher.
+--  The columns of the table indicate: [Algorithm name, Depth of solution, 
+--  Cost of solution, Number of goal checks, Number of node expansions,
+--  Number of states expanded] .
 detailedCompareSearchers ::
-        [ProblemIO p s a -> Maybe (Node s1 a1)]
-     -> [String]
-     -> p s a
+        [ProblemIO p s a -> Maybe (Node s1 a1)] -- ^ List of searchers
+     -> [String]                                -- ^ Names of searchers
+     -> p s a                                   -- ^ Problem
      -> IO ()
 detailedCompareSearchers searchers names prob = do
     result <- testSearchers searchers prob
@@ -459,6 +501,7 @@ detailedCompareSearchers searchers names prob = do
         header = ["Searcher","Depth","Cost","Goal Checks","Successors",
                   "States"]
 
+-- |Run all search algorithms over a few example problems.
 compareGraphSearchers :: IO ()
 compareGraphSearchers = do
     compareSearchers searchers probs header rownames
@@ -474,6 +517,8 @@ compareGraphSearchers = do
                  , "Iterative Deepening Search", "MinimumCostSearch"
                  , "GreedyBestFirstSearch", "A* Search"]
 
+-- |Run all search algorithms over a particular problem and print out
+--  performance statistics.
 runDetailedCompare :: (Problem p s a, Ord s, Show s) => p s a -> IO ()
 runDetailedCompare = detailedCompareSearchers allSearchers allSearcherNames
 
