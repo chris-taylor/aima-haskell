3a09fb7d798552a2f1c362d6a5ab8c27515dd248
Chris Taylor <crntaylor@gmail.com>
2012-07-05 10:46:54 +0100

    Added documentation

--- src/AI/Games.hs
+++ src/AI/Games.hs
@@ -358,18 +358,22 @@ instance Game TicTacToe TTState TTMove where
 
     terminalTest g s = utilityTT s /= 0 || null (legalMoves g s)
 
--- |Helper function that computes 
+-- |Helper function that computes the utility of a state after a particular
+--  move is played.
 computeUtility :: TTState -> TTMove -> Utility
 computeUtility s@(TTS _ player _ _) move = if kInARow s move player
     then if player == O then 1 else -1
     else 0
 
--- |ss
+-- |Given the current state of the board, return @True@ if putting a counter
+--  into a specific square would win the game.
 kInARow :: TTState -> TTMove -> TTCounter -> Bool
 kInARow state move player = f (1,0) || f (0,1) || f (1,1) || f (1,-1)
     where
         f = kInARow' state move player
 
+-- |A helper function for 'kInARow'. Given a state of the board, does adding a
+--  counter to a specific square given k-in-a-row in the specified direction?
 kInARow' :: TTState -> TTMove -> TTCounter -> (Int,Int) -> Bool
 kInARow' (TTS board _ _ (_,_,k)) (x,y) p (dx,dy) = n1 + n2 - 1 >= k
     where
@@ -401,11 +405,16 @@ toChars (TTS board _ _ (h,v,_)) = reverse $ map (map f) board'
 ------------
 -- Connect 4
 
+-- |The 'Connect4' data type is a wrapper around 'TicTacToe', which allows us
+--  to inherit most of its behaviour.
 data Connect4 s a = C (TicTacToe s a)
 
+-- |The standard Connect 4 board.
 connect4 :: Connect4 TTState TTMove
 connect4 = C (TTT 7 6 4)
 
+-- |A Connect 4 game is identical to tic tac toe in most respects. It differs in
+--  the set of legal moves, and the heuristic function.
 instance Game Connect4 TTState TTMove where
     initial      (C g)        = initial g
     toMove       (C g) s      = toMove g s
@@ -418,39 +427,53 @@ instance Game Connect4 TTState TTMove where
 
     heuristic _ = heuristicC4 [0.1,-0.1,1,-1]
 
--- Compute heuristics for Connect 4
-
-toListRep :: TTState -> [[Maybe TTCounter]]
-toListRep (TTS board _ _ (h,v,_)) =
-    [[ M.lookup (i,j) board | i <- [0..h-1] ] | j <- [0..v-1]]
-
--- Winning lines
-
+-- |A heuristic function for Connect 4. If the game is won or lost, a value of
+--  positive or negative infinity is assigned. Otherwise the value is a weighted
+--  combination of simpler heuristic functions.
 heuristicC4 :: [Double] -> TTState -> Player -> Utility
 heuristicC4 weights s p
     | u > 0 = posInf
     | u < 0 = negInf
-    | otherwise = sum $ zipWith (*) weights (map fromIntegral [n1,n2,n3,n4])
+    | otherwise = sum $ zipWith (*) weights [n1,n2,n3,n4]
     where
         u  = if p == Max then utilityTT s else negate (utilityTT s)
-        n1 = numWinningLines p s
-        n2 = numWinningLines (opponent p) s
-        n3 = numThreats p s
-        n4 = numThreats (opponent p) s
+        n1 = fromIntegral $ numWinningLines p s
+        n2 = fromIntegral $ numWinningLines (opponent p) s
+        n3 = fromIntegral $ numThreats p s
+        n4 = fromIntegral $ numThreats (opponent p) s
 
+-- |Compute the number of winning lines heuristic for a particular player. A
+--  winning line is defined to be a line of k squares, which contains at least
+--  one of your counters, and none of the opponents counters.
 numWinningLines :: Player -> TTState -> Int
-numWinningLines p s = length $ filter (isWinningLine $ counter p) allLines
-    where
-        allLines = concat [ linesInDir s (1,0), linesInDir s (0,1)
-                          , linesInDir s (1,1), linesInDir s (1,-1) ]
-        counter Max = O
-        counter Min = X
+numWinningLines p s = length $ filter (isWinningLine $ counter p) (allLines s)
 
+-- |Compute the number of threats heuristic for a particular player. A threat
+--  cell is a cell that would win the game if it was filled in, but is not
+--  blockable on the next move (i.e. it is not on the bottom row and does not
+--  have a counter directly beneath it).
+numThreats :: Player -> TTState -> Int
+numThreats p s@(TTS _ _ _ (h,v,_)) = length $ filter (isThreat s p) xs
+    where
+        xs = [ (i,j) | i <- [0..h-1], j <- [0..v-1] ]
+
+-- |Return @True@ is a specic cell is a threat cell.
+isThreat :: TTState -> Player -> (Int,Int) -> Bool
+isThreat s@(TTS board _ _ _) p (x,y) =
+    y /= 0 && (x,y-1) `M.notMember` board && kInARow s (x,y) (counter p)
+
+-- |Return @True@ if a line of pieces is a winning line.
 isWinningLine :: TTCounter -> [Maybe TTCounter] -> Bool
 isWinningLine c xs = c `elem` ys && not (other c `elem` ys)
     where
         ys = catMaybes xs
 
+-- |Return a list of all of the lines on the board.
+allLines :: TTState -> [[Maybe TTCounter]]
+allLines s = concat [ linesInDir s (1,0), linesInDir s (0,1)
+                    , linesInDir s (1,1), linesInDir s (1,-1) ]
+
+-- |Return all of the lines on the board in the specified direction.
 linesInDir :: TTState -> (Int,Int) -> [[Maybe TTCounter]]
 linesInDir s@(TTS board _ _ (h,v,k)) dir = 
     map (\p -> lineThrough s p dir) pts
@@ -461,17 +484,7 @@ linesInDir s@(TTS board _ _ (h,v,k)) dir =
             (1,1)  -> [ (x,y) | x <- [0..h-k], y <- [0..v-k] ]
             (1,-1) -> [ (x,y) | x <- [0..h-k], y <- [k-1..v-1] ]
 
+-- |Return the line starting in cell (x,y) and continuing in direction (dx,dy)
 lineThrough :: TTState -> (Int,Int) -> (Int,Int) -> [Maybe TTCounter]
 lineThrough (TTS board _ _ (h,v,k)) (x,y) (dx,dy) = 
     take k $ map (`M.lookup` board) ( zip [x,x+dx..] [y,y+dy..] )
-
--- Number of threats
-
-numThreats :: Player -> TTState -> Int
-numThreats p s@(TTS _ _ _ (h,v,_)) = length $ filter (isThreat s p) xs
-    where
-        xs = [ (i,j) | i <- [0..h-1], j <- [0..v-1] ]
-
-isThreat :: TTState -> Player -> (Int,Int) -> Bool
-isThreat s@(TTS board _ _ _) p (x,y) =
-    y /= 0 && (x,y-1) `M.notMember` board && kInARow s (x,y) (counter p)
