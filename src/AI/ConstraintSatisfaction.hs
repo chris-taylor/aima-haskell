af78cd02073799069215dd0d08a913a9411e0137
Chris Taylor <crntaylor@gmail.com>
2012-07-10 23:38:03 +0100

    tidying up

--- src/AI/ConstraintSatisfaction.hs
+++ src/AI/ConstraintSatisfaction.hs
@@ -146,8 +146,8 @@ recursiveBacktracking csp = getAssignment >>= \assgn ->
     if allAssigned csp assgn
         then return (Just assgn)
         else do
-            var  <- selectUnassignedVariable (vars csp)
-            vals <- orderDomainValues csp var
+            var  <- selectVariable (vars csp)
+            vals <- sortDomainVals csp var
             go var vals
         where
             go var []     = return Nothing
@@ -165,10 +165,14 @@ recursiveBacktracking csp = getAssignment >>= \assgn ->
 -- |Select an unassigned variable from the list of variables in a CSP. We may
 --  optionally use the Most Constrained Variable heuristic to choose which
 --  variable to assign next.
-selectUnassignedVariable :: Ord v => [v] -> Backtracking v a v
-selectUnassignedVariable vs = ifM useMcv
-    (mostConstrained vs)
-    (firstUnassigned vs)
+selectVariable :: Ord v => [v] -> Backtracking v a v
+selectVariable vs = ifM useMcv (mostConstrained vs) (firstUnassigned vs)
+
+-- |Given a variable in a CSP, select an order in which to try the allowed
+--  values for that variable. We may optionally use the Least Constraining
+--  Variable heuristic to try values with fewest conflicts first.
+sortDomainVals :: CSP c v a => c v a -> v -> Backtracking v a [a]
+sortDomainVals csp var = ifM useLcv (leastConstraining csp var) (allValues var)
 
 -- |Return the most constrained variable from a problem. The idea is to speed
 --  up the search algorithm by reducing the branching factor.
@@ -177,22 +181,15 @@ mostConstrained vs = do
     dom   <- getDomain
     assgn <- getAssignment
     let unassigned = [ v | v <- vs, v `M.notMember` assgn ]
-    return (argMin unassigned $ negate . numLegalValues dom)
+    return (argMin unassigned $ numLegalValues dom)
 
 -- |Return the first unassigned variable in the problem.
 firstUnassigned :: Ord v => [v] -> Backtracking v a v
-firstUnassigned (v:vs) = getAssignment >>= \assgn ->
-    if v `M.notMember` assgn
-        then return v
-        else selectUnassignedVariable vs
-
--- |Given a variable in a CSP, select an order in which to try the allowed
---  values for that variable. We may optionally use the Least Constraining
---  Variable heuristic to try values with fewest conflicts first.
-orderDomainValues :: CSP c v a => c v a -> v -> Backtracking v a [a]
-orderDomainValues csp var = ifM useLcv
-    (leastConstraining csp var)
-    (allValues var)
+firstUnassigned (v:vs) = getAssignment >>= select vs
+    where
+        select vs assgn = if v `M.notMember` assgn
+            then return v
+            else firstUnassigned vs
 
 -- |Return a list of values for a given variable, sorted in order of least
 --  constraining to most constraining.
