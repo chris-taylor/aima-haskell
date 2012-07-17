d939f95844aac27f8e30518d533e684b0d253f95
Chris Taylor <crntaylor@gmail.com>
2012-07-17 10:13:30 +0100

    Added error handling

--- src/AI/Logic/Main.hs
+++ src/AI/Logic/Main.hs
@@ -1,6 +1,7 @@
 module AI.Logic.Main where
 
 import Control.Monad
+import Control.Monad.Error
 import Control.Monad.State
 
 import qualified Data.List as L
@@ -13,39 +14,61 @@ import AI.Util.Util
 -- Interactive Code --
 ----------------------
 
-run :: IO ()
+data LogicError  = ParseError | UnknownCommand | DefaultError
+
+instance Error LogicError where noMsg = DefaultError
+
+type ThrowsError = ErrorT LogicError IO
+type KB          = StateT [Expr] ThrowsError
+
+run :: IO (Either LogicError ())
 run = do
     putStrLn "Propositional Logic Theorem Prover"
-    evalStateT loop []
+    runErrorT $ evalStateT loop []
 
-loop :: StateT [Expr] IO ()
+loop :: KB ()
 loop = do
     liftIO $ putStr "> "
     (cmd, rest) <- fmap (break (==' ')) (liftIO getLine)
     case cmd of
-        "quit"  -> return ()
-        "tell"  -> tell (strip rest) >> loop
-        "ask"   -> ask (strip rest) >> loop
-        "show"  -> get >>= (\ps -> liftIO $ showPremises ps) >> loop
-        "help"  -> liftIO showHelp >> loop
-        "clear" -> put [] >> loop
-        ""      -> loop
-        _       -> liftIO unknown >> loop
+        "show"      -> get >>= (\ps -> liftIO $ showPremises ps) >> loop
+        "help"      -> liftIO showHelp >> loop
+        "tell"      -> parse rest >>= tell >> loop
+        "ask"       -> parse rest >>= ask >> loop
+        "retract"   -> parse rest >>= retract >> loop
+        "clear"     -> clear >> loop
+        "quit"      -> return ()
+        ""          -> loop
+        _           -> unknown >> loop
 
-tell :: String -> StateT [Expr] IO ()
-tell expr = case parseExpr expr of
-    Nothing -> liftIO (putStrLn "***parseError")
-    Just p  -> modify (associate p:)
+parse :: String -> KB Expr
+parse str = case parseExpr (strip str) of
+    Nothing -> liftIO (putStrLn "***parse error") >> throwError ParseError
+    Just p  -> return (associate p)
 
-ask :: String -> StateT [Expr] IO ()
-ask expr = case parseExpr expr of
-    Nothing -> liftIO (putStrLn "*** parse error") >> loop
-    Just q  -> do
-        ps <- get
-        liftIO $ showPremises ps
-        if plResolution (And ps) q
-            then liftIO $ putStrLn $ "Entailed: " ++ show q
-            else liftIO $ putStrLn $ "Not entailed: " ++ show q
+tell :: Expr -> KB ()
+tell expr = modify (++[expr])
+
+ask :: Expr -> KB ()
+ask expr = do
+    ps <- get
+    liftIO $ showPremises ps
+    if plResolution (And ps) expr
+        then liftIO $ putStrLn $ "Entailed: " ++ show expr
+        else liftIO $ putStrLn $ "Not entailed: " ++ show expr
+
+retract :: Expr -> KB ()
+retract expr = do
+    ps <- get
+    if expr `elem` ps
+        then modify $ L.delete expr
+        else liftIO $ putStrLn $ "***expression " ++ show expr ++ " not in KB"
+
+clear :: KB ()
+clear = put []
+
+unknown :: KB ()
+unknown = liftIO (putStrLn "***unknown command") >> throwError UnknownCommand
 
 showPremises :: [Expr] -> IO ()
 showPremises ps = forM_ (enumerate ps) $
@@ -53,11 +76,10 @@ showPremises ps = forM_ (enumerate ps) $
 
 showHelp :: IO ()
 showHelp = do
-    putStrLn "  quit   - Exit the PLTP"
-    putStrLn "  tell P - Store proposition P in the knowledge base"
-    putStrLn "  ask P  - Ask whether P is entailed by the knowledge base"
-    putStrLn "  clear  - Empty the knowledge base"
-    putStrLn "  show   - Display the knowldge base"
-
-unknown :: IO ()
-unknown = putStrLn "***unknown command"
+    putStrLn "  tell P    Store proposition P in the knowledge base"
+    putStrLn "  retract P Remove proposition P from the knowledge base"
+    putStrLn "  ask P     Ask whether P is entailed by the knowledge base"
+    putStrLn "  clear     Remove all propositions from the knowledge base"
+    putStrLn "  show      Display the current state of the knowledge base"
+    putStrLn "  help      Show this help file"
+    putStrLn "  quit      Exit the PLTP"
