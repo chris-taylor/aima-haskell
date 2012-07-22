1d319f4d24b9999124e09a7ddac24695b0946813
Chris Taylor <crntaylor@gmail.com>
2012-07-22 15:29:24 +0100

    parser for first-order logic

--- src/AI/Logic/FOL.hs
+++ src/AI/Logic/FOL.hs
@@ -1,6 +1,15 @@
 module AI.Logic.FOL where
 
+import Control.Monad.Error
+import Text.ParserCombinators.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Expr
+--import qualified Text.Parsec.Token
+
 import qualified Data.List as L
+
+import AI.Logic.Core
+import AI.Util.Util
 
 ----------------
 -- Data Types --
@@ -8,6 +17,7 @@ import qualified Data.List as L
 
 data Term = Var String
           | Func String [Term]
+          deriving (Eq)
 
 data FOLExpr = Val Bool
              | Pred String [Term]
@@ -17,6 +27,7 @@ data FOLExpr = Val Bool
              | Implies FOLExpr FOLExpr
              | ForAll String FOLExpr
              | Exists String FOLExpr
+             deriving (Eq)
 
 instance Show Term where
     show (Var x)     = x
@@ -33,6 +44,102 @@ instance Show FOLExpr where
     show (Exists x p)  = "exists " ++ x ++ ". " ++ show p
     show (Pred p xs)   = p ++ "(" ++ commaSep (map show xs) ++  ")"
 
-commaSep :: [String] -> String
-commaSep xs = concat $ L.intersperse "," xs
+instance Expr FOLExpr where
+    parseExpr str = case parseFOL str of
+        Nothing -> throwError ParseError
+        Just e  -> return e
+
+------------------------------
+-- First Order Logic Parser --
+------------------------------
+
+-- |Parse a 'String' as an expression in propositional logic.
+parseFOL :: String -> Maybe FOLExpr
+parseFOL input = case parse expr "" input of
+    Left _  -> Nothing
+    Right x -> return x
+
+expr :: Parser FOLExpr
+expr = buildExpressionParser table term <?> "expression"
+
+term :: Parser FOLExpr
+term = parens expr
+   <|> try parseVal
+   <|> try parsePred
+   <|> parseForAll
+   <|> parseExists
+   <?> "T, F or expression"
+
+
+-- |Parse a first-order logic term - either a variable or a function.
+parseTerm :: Parser Term
+parseTerm = parseVar <|> parseFunc <?> "Variable or Function"
+
+-- |Parse a variable in first-order logic.
+parseVar :: Parser Term
+parseVar = do
+    x <- name
+    return $ Var x
+
+-- |Parse a function in first-order logic.
+parseFunc :: Parser Term
+parseFunc = do
+    x  <- name
+    ts <- parens (parseTerm `sepBy` char ',')
+    return $ Func x ts
+
+-- |Parse a boolean value in first-order logic.
+parseVal :: Parser FOLExpr
+parseVal = (char 'T' >> return (Val True))
+       <|> (char 'F' >> return (Val False))
+       <?> "T or F"
+
+-- |Parse a predicate in first-order logic.
+parsePred :: Parser FOLExpr
+parsePred = do
+    x  <- name
+    ts <- parens (parseTerm `sepBy` char ',')
+    return $ Pred x ts
+
+-- |Parse a forall statement in first-order logic.
+parseForAll :: Parser FOLExpr
+parseForAll = do
+    string "forall" >> spaces
+    x <- name
+    char '.' >> spaces
+    e <- expr
+    return $ ForAll x e
+
+-- |Parse an /exists/ statement in first-order logic.
+parseExists :: Parser FOLExpr
+parseExists = do
+    string "exists" >> spaces
+    x <- name
+    char '.' >> spaces
+    e <- expr
+    return $ Exists x e
+
+
+table = 
+    [ [prefix "~" Not]
+    , [binary "&" (\x y -> And [x,y]) AssocLeft]
+    , [binary "|" (\x y -> Or [x,y]) AssocLeft]
+    , [binary "=>" Implies AssocRight] ]
+
+binary name fun assoc = Infix (do{ string name; return fun }) assoc
+prefix name fun       = Prefix (do{ string name; return fun })
+
+
+name :: Parser String
+name = do
+    c  <- letter
+    cs <- many alphaNum
+    return (c:cs)
+
+parens :: Parser a -> Parser a
+parens p = do
+    char '(' >> spaces
+    x <- p
+    spaces >> char ')'
+    return x
 
