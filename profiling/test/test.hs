eb22bb21428050fe82d39e3b9b73bd4782b5e65a
Chris Taylor <crntaylor@gmail.com>
2012-07-22 15:41:03 +0100

    removed test files

--- profiling/test/test.hs
+++
@@ -1,9 +0,0 @@
-import AI.Probability.Example.Grass
-import AI.Probability.Bayes
-import System.Environment (getArgs)
-  
-main = do 
-    args <- getArgs
-    let n = read (head args) :: Int
-    test <- likelihoodWeighting n grass [] "Rain"  
-    print test
