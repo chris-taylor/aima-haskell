faf949cb079b9193a2dde60ea82feb07c95595e5
Chris Taylor <crntaylor@gmail.com>
2012-07-31 20:04:07 +0100

    Profiling the Monte Carlo code in Restaurants example

---
+++ profiling/Restaurants/Restaurants.hs
@@ -0,0 +1,4 @@
+import AI.Learning.Example.Restaurant
+
+main :: IO ()
+main = demo 0.0
---
+++ profiling/Restaurants/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Reinstall library
+cd ../..
+cabal install -O2 -p -fprof-auto 
+cd "profiling/Restaurants/"
+
+# Compile test file
+ghc -rtsopts -prof -fprof-auto -fforce-recomp --make -O2 "Restaurants.hs"
+
+# Run with profiling enabled and view output
+./Restaurants +RTS -K100M -s -p -hy
+cat "Restaurants.prof"
+hp2ps -c "Restaurants.hp"
+open "Restaurants.ps"
+
+# Clean up files from last compilation
+rm *.hi *.o *.aux
+rm Restaurants
