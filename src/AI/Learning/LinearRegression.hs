654f351b802f15b2664277fb83eb42c89fa5dc22
Chris Taylor <crntaylor@gmail.com>
2012-08-05 18:19:08 +0100

    Work on linear regression module

--- src/AI/Learning/LinearRegression.hs
+++ src/AI/Learning/LinearRegression.hs
@@ -4,6 +4,32 @@ import Data.Packed.Matrix
 import Data.Packed.Vector
 import Numeric.LinearAlgebra
 
---regress :: Floating a 
-regress y x = inv (trans x <> x) <> trans x <> y
+import AI.Util.Matrix
 
+data LROpts = LROpts { useConst :: Bool }
+
+stdLROpts :: LROpts
+stdLROpts = LROpts { useConst = True }
+
+-- |Regress a vector y against a matrix of predictors x. Returns a coefficient
+--  vector.
+regress :: Field t => Vector t -> Matrix t -> Vector t
+regress = regressWith stdLROpts
+
+regressWith :: Field t => LROpts -> Vector t -> Matrix t -> Vector t
+regressWith opts y x =
+    let x1 = if useConst opts
+                then addOnes x
+                else x
+        (_,m) = size x1
+        (_,r) = qr x1
+        r1 = takeRows m r
+     in (trans r1 <> r1) <\> trans x1 <> y
+
+
+---------------
+-- Utilities --
+---------------
+
+addOnes :: (Num a, Element a) => Matrix a -> Matrix a
+addOnes x = horzcat [ones (rows x, 1), x]
--- src/AI/Util/Matrix.hs
+++ src/AI/Util/Matrix.hs
@@ -1,72 +1,29 @@
 module AI.Util.Matrix where
 
-import Data.Array
-import qualified Data.List as L
+import Foreign.Storable (Storable)
+import Data.Packed.Matrix
+import Data.Packed.Vector
 
-newtype Matrix a = M { unM :: Array (Int,Int) a }
+-- |Create an @nxm@ matrix with every element identical.
+constantMatrix :: Storable a => (Int,Int) -> a -> Matrix a
+constantMatrix (n,m) x = (n><m) (repeat x)
 
-instance Show a => Show (Matrix a) where
-    show = concat . L.intersperse "\n" . showMat
+-- |Create an @nxm@ matrix of zeros.
+zeros :: (Num a, Storable a) => (Int,Int) -> Matrix a
+zeros sz = constantMatrix sz 0
 
-showMat :: Show a => Matrix a -> [String]
-showMat (M x) = [ unwords $ map show
-                    [ x!(i,j) | i <- range (li,ui) ]
-                |  j <- range(lj,uj) ]
-    where
-        ((li,lj),(ui,uj)) = bounds x
+-- |Create an @nxm@ matrix of ones.
+ones :: (Num a, Storable a) => (Int,Int) -> Matrix a
+ones sz = constantMatrix sz 1
 
-isScalar :: Matrix a -> Bool
-isScalar (M x) = rangeSize (bounds x) == 1
+-- |Return the size of a matrix as a 2-tuple.
+size :: Matrix a -> (Int,Int)
+size x = (rows x, cols x)
 
-instance Num a => Num (Matrix a) where
-    (+) = addM
-    (*) = mulM
-    negate = M . fmap negate . unM
-    signum = M . fmap signum . unM
-    abs    = M . fmap abs . unM
-    fromInteger n = M $ array ((1,1),(1,1)) [((1,1),fromInteger n)]
+-- |Concatenate matrices horizontally.
+horzcat :: Element a => [Matrix a] -> Matrix a
+horzcat = fromBlocks . return
 
-addM :: Num a => Matrix a -> Matrix a -> Matrix a
-addM (M x) (M y) = M $ array resultBounds
-                         [ (i, x!i + y!i) | i <- range resultBounds ]
-        where resultBounds
-                | bnds==bnds'   = bnds
-                | otherwise     = error "addM: incompatible bounds"
-              bnds  = bounds x
-              bnds' = bounds y
-
-mulM :: Num a => Matrix a -> Matrix a -> Matrix a
-mulM x y
-    | isScalar x = scalarMulM x y
-    | isScalar y = scalarMulM y x
-    | otherwise  = mulM' x y
-
-scalarMulM :: Num a => Matrix a -> Matrix a -> Matrix a
-scalarMulM (M s) (M x) = M $ fmap (* s!(1,1)) x
-
-mulM' :: Num a => Matrix a -> Matrix a -> Matrix a
-mulM' (M x) (M y) = M $ accumArray (+) 0 resultBounds
-                            [((i,j), x!(i,k) * y!(k,j))
-                                      | i <- range (li,ui),
-                                        j <- range (lj',uj'),
-                                        k <- range (lj,uj)  ]
-        where ((li,lj),(ui,uj))         = bounds x
-              ((li',lj'),(ui',uj'))     = bounds y
-              resultBounds
-                | (lj,uj)==(li',ui')    = ((li,lj'),(ui,uj'))
-                | otherwise             = error "mulM: incompatible bounds"
-
-mkArray :: Ix i => (i -> a) -> (i,i) -> Array i a
-mkArray f bnds = array bnds [ (i, f i) | i <- range bnds ]
-
-constArray :: (Int,Int) -> a -> Array (Int,Int) a
-constArray bnds a = mkArray (const a) ((1,1),bnds)
-
-eye :: Num a => Int -> Matrix a
-eye n = M $ mkArray (\(i,j) -> if i == j then 1 else 0) ((1,1),(n,n))
-
-ones :: Num a => (Int,Int) -> Matrix a
-ones bnds = M $ constArray bnds 1
-
-zeros :: Num a => (Int,Int) -> Matrix a
-zeros bnds = M $ constArray bnds 0
+-- |Concatenate matrices vertically.
+vertcat :: Element a => [Matrix a] -> Matrix a
+vertcat = fromBlocks . map return
