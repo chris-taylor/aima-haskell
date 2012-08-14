96ec4d39b2e25de9ef629e111d90f4977f8c3f28
Chris Taylor <crntaylor@gmail.com>
2012-08-13 20:03:27 -0500

    Modified linear regression to apply only to matrices of Double

--- src/AI/Learning/LinearRegression.hs
+++ src/AI/Learning/LinearRegression.hs
@@ -40,50 +40,50 @@ A linear model should contain the following information:
 -- |Type recording what kind of linear model we want to build. Choose from
 --  ordinary least squares (OLS), ridge regression with parameter lambda
 --  and LASSO with parameter lambda.
-data LMType t = OLS
-              | Ridge t
-              | LASSO t
-              deriving (Show)
+data LMType = OLS
+            | Ridge Double
+            | LASSO Double
+            deriving (Show)
 
--- |Data type for linear regression options.
+---- |Data type for linear regression options.
 data LMOpts = LMOpts { fitIntercept :: Bool
                      , standardizeRegressors :: Bool }
                      deriving (Show)
 
--- |Standard options for linear regression (used by 'regress').
+---- |Standard options for linear regression (used by 'regress').
 stdLMOpts = LMOpts { fitIntercept = True
                    , standardizeRegressors = False }
 
--------------------
--- Linear Models --
--------------------
+---------------------
+---- Linear Models --
+---------------------
 
-data LinearModel t = LM { coefs   :: Vector t
-                        , lmIntercept :: Bool
-                        , lmStandardize :: Bool
-                        , lmMean :: Vector t
-                        , lmStd  :: Vector t }
-                        deriving (Show)
+data LinearModel = LM { coefs   :: Vector Double
+                      , lmIntercept :: Bool
+                      , lmStandardize :: Bool
+                      , lmMean :: Vector Double
+                      , lmStd  :: Vector Double }
+                      deriving (Show)
 
-data LMStats t = LMStats { covBeta :: Maybe (Matrix t)
-                         , betaCI :: Maybe (Matrix t)
-                         , sst :: t
-                         , sse :: t
-                         , mse :: t
-                         , rSquare :: t
-                         , tBeta :: Maybe (Vector t)
-                         , pBeta :: Maybe (Vector t)
-                         , fRegression :: Maybe t
-                         , pRegression :: Maybe t }
-                         deriving (Show)
+data LMStats = LMStats { covBeta :: Maybe (Matrix Double)
+                       , betaCI :: Maybe (Matrix Double)
+                       , sst :: Double
+                       , sse :: Double
+                       , mse :: Double
+                       , rSquare :: Double
+                       , tBeta :: Maybe (Vector Double)
+                       , pBeta :: Maybe (Vector Double)
+                       , fRegression :: Maybe Double
+                       , pRegression :: Maybe Double }
+                       deriving (Show)
 
-lm :: (Floating (Vector t), Field t) => Matrix t -> Vector t -> LinearModel t
+lm :: Matrix Double -> Vector Double -> LinearModel
 lm = lmWith OLS stdLMOpts
 
-lmRidge :: (Floating (Vector t), Field t) => Matrix t -> Vector t -> t -> LinearModel t
+lmRidge :: Matrix Double -> Vector Double -> Double -> LinearModel
 lmRidge x y lambda = lmWith (Ridge lambda) stdLMOpts x y
 
-lmWith :: (Floating (Vector t), Field t) => LMType t -> LMOpts -> Matrix t -> Vector t -> LinearModel t
+lmWith :: LMType -> LMOpts -> Matrix Double -> Vector Double -> LinearModel
 lmWith kind opts x y = LM { coefs = beta
                           , lmIntercept = fitIntercept opts
                           , lmStandardize = standardizeRegressors opts
@@ -96,21 +96,20 @@ lmWith kind opts x y = LM { coefs = beta
             Ridge a -> ridgeRegress xx y (fitIntercept opts) a
             LASSO a -> error "LASSO not implemented."
 
-lmPrepare :: (Floating (Vector t), Fractional t, Element t, Container Vector t) =>
-             LMOpts
-          -> Matrix t
-          -> (Matrix t, Vector t, Vector t)
+lmPrepare :: LMOpts
+          -> Matrix Double
+          -> (Matrix Double, Vector Double, Vector Double)
 lmPrepare opts x = (x3,mu,sigma) 
     where
         (x1,mu,sigma) = standardize x
         x2            = if standardizeRegressors opts then x1 else x
         x3            = if fitIntercept opts then addOnes x2 else x2
 
--------------------------------
--- Predict from linear model --
--------------------------------
+---------------------------------
+---- Predict from linear model --
+---------------------------------
 
-lmPredict :: (Floating (Vector t), Field t) => LinearModel t -> Matrix t -> Vector t
+lmPredict :: LinearModel -> Matrix Double -> Vector Double
 lmPredict model xx = x2 <> beta
     where
         beta    = coefs model
@@ -123,7 +122,7 @@ lmPredict model xx = x2 <> beta
                     then addOnes x1
                     else x1
 
-lmStats :: (Floating (Vector t), Field t) => LinearModel t -> Matrix t -> Vector t -> LMStats t
+lmStats :: LinearModel -> Matrix Double -> Vector Double -> LMStats
 lmStats model x y =
     let ybar      = constant (mean y) (dim y)
         yhat      = lmPredict model x
@@ -140,16 +139,15 @@ lmStats model x y =
         pReg      = Nothing
     in LMStats covBeta betaCI sst sse mse rSq tBeta pBeta fReg pReg
 
-------------------------
--- Perform Regression --
-------------------------
+--------------------------
+---- Perform Regression --
+--------------------------
 
--- |Regress a vector y against a matrix of predictors x, with the specified
---  options.
-regress :: (Num (Vector t), Field t) =>
-            Matrix t -- X
-         -> Vector t -- y
-         -> Vector t -- beta
+---- |Regress a vector y against a matrix of predictors x, with the specified
+----  options.
+regress :: Matrix Double -- X
+        -> Vector Double -- y
+        -> Vector Double -- beta
 regress x y
     | rows x /= dim y = error "Inconsistent dimensions -- REGRESS"
     | otherwise = let (_,n) = size x
@@ -157,12 +155,13 @@ regress x y
                       rr = takeRows n r
                    in (trans rr <> rr) <\> trans x <> y
 
-ridgeRegress :: (Num (Vector t), Field t) =>
-                Matrix t -- X
-             -> Vector t -- y
-             -> Bool     -- useConst
-             -> t        -- lambda
-             -> Vector t -- beta
+-- |Ridge regression. This adds a penalty term to OLS regression, which
+--  discourages large coefficient values to prevent overfitting.
+ridgeRegress :: Matrix Double  -- X
+             -> Vector Double  -- y
+             -> Bool           -- useConst?
+             -> Double         -- lambda
+             -> Vector Double  -- beta
 ridgeRegress x y useConst lambda
     | rows x /= dim y = error "Inconsistent dimensions -- RIDGEREGRESS"
     | otherwise = let (_,n) = size x
@@ -173,22 +172,20 @@ ridgeRegress x y useConst lambda
                             else ident n
                    in (trans rr <> rr + lambda `scale` ww) <\> trans x <> y
 
----------------
--- Utilities --
----------------
+-----------------
+---- Utilities --
+-----------------
 
 -- |Add a column of ones to a matrix.
-addOnes :: (Num a, Container Vector a) => Matrix a -> Matrix a
+addOnes :: Matrix Double -> Matrix Double
 addOnes x = horzcat [ones (rows x) 1, x]
 
 -- |De-mean a sample.
-demean :: (Num (Vector t), Field t) => Matrix t -> Matrix t
+demean :: Matrix Double -> Matrix Double
 demean x = eachRow (subtract $ mean x) x
 
 -- |Standardize a sample to have zero mean and unit variance.
-standardize :: (Floating (Vector t), Fractional t, Element t) =>
-               Matrix t                         -- Data sample
-            -> (Matrix t, Vector t, Vector t)   -- Standardized data, mu, sigma
+standardize :: Matrix Double -> (Matrix Double, Vector Double, Vector Double)
 standardize m = (eachRow (\x -> (x - mu) / sigma) m, mu, sigma)
     where mu    = mean m
           sigma = std m
@@ -197,32 +194,27 @@ standardize m = (eachRow (\x -> (x - mu) / sigma) m, mu, sigma)
 -- Mean, Variance etc. --
 -------------------------
 
-class Statistic a b | a -> b where
+class Mean a b | a -> b where
     mean :: a -> b
-    sum :: a -> b
+
+class Floating b => Variance a b | a -> b where
     var :: a -> b
+    var x = y * y where y = std x
 
-std :: Floating b => Statistic a b => a -> b
-std x = sqrt (var x)
+    std :: a -> b
+    std x = sqrt $ var x
 
-instance Fractional t => Statistic [t] t where
-    mean xs = xsum / xlen
-        where (xsum,xlen)   = foldl' fun (0,0) xs
-              fun (!a,!b) x = (a+x,b+1)
-    sum xs = foldl' (+) 0 xs
-    var xs = mean $ map ((^ 2) . subtract xbar) xs
-        where
-            xbar = mean xs
-
-instance (Num (Vector t), Fractional t, Element t) => Statistic (Vector t) t where
+instance Mean (Vector Double) Double where
     mean v = sumVector v / fromIntegral (dim v)
-    sum v  = sumVector v
+
+instance Variance (Vector Double) Double where
     var v  = mean $ (v - constant vbar (dim v)) ^ 2
         where vbar = mean v
 
-instance (Num (Vector t), Fractional t, Element t) => Statistic (Matrix t) (Vector t) where
+instance Mean (Matrix Double) (Vector Double) where
     mean m = fromList $ mapCols mean m
-    sum m  = fromList $ mapCols sum m
+
+instance Variance (Matrix Double) (Vector Double) where
     var m  = fromList $ mapCols var m
 
 
