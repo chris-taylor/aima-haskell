d06f3af2fb4c9e95908988aa4d5a0d624163a462
Chris Taylor <crntaylor@gmail.com>
2012-08-19 11:45:24 -0500

    regularized logistic regression (needs cleaning up)

--- src/AI/Learning/LogisticRegression.hs
+++ src/AI/Learning/LogisticRegression.hs
@@ -32,6 +32,25 @@ lr y x = fst $ minimizeVD VectorBFGS2 prec niter sz1 tol cost grad theta0
         grad    = negate . snd . lrLogLikelihood y x
         theta0  = constant 0 (cols x)
 
+-- |Regularized logistic regression with quadratic penalty on the coefficients.
+--  You should standardize the coefficients of the design matrix before using
+--  this function, as the regularization procedure is sensitive to the scale
+--  of the predictors. See also 'lr'.
+lrRegularized :: Vector Double  -- targets (y)
+              -> Matrix Double  -- design matrix (x)
+              -> Bool           -- first column of design matrix is all ones?
+              -> Double         -- regularization constant (lambda)
+              -> Vector Double  -- coefficient vector (theta)
+lrRegularized y x useConst lambda = fst $ minimizeVD VectorBFGS2 prec niter sz1 tol cost grad theta0
+    where
+        prec    = 1e-9
+        niter   = 1000
+        sz1     = 0.1
+        tol     = 0.1
+        cost    = negate . fst . lrLogLikRegularized y x useConst lambda
+        grad    = negate . snd . lrLogLikRegularized y x useConst lambda
+        theta0  = constant 0 (cols x)
+
 -- |Cost function and derivative for logistic regression. This is maximized when
 --  fitting parameters for the regression.
 lrLogLikelihood :: Vector Double             -- targets (y)
@@ -43,7 +62,26 @@ lrLogLikelihood y x theta = (cost, grad)
         m    = fromIntegral (rows x)    -- For computing average
         h    = sigmoid $ x <> theta     -- Predictions for y
         cost = sumVector (y * log h + (1-y) * log (1-h)) / m
-        grad = (1/m) `scale` (y - h) <> x;
+        grad = (1/m) `scale` (y - h) <> x
+
+-- |Cost function and derivative for regularized logistic regression. This is
+--  maximized when fitting parameters for the regression.
+lrLogLikRegularized :: Vector Double
+                    -> Matrix Double
+                    -> Bool
+                    -> Double
+                    -> Vector Double
+                    -> (Double, Vector Double)
+lrLogLikRegularized y x useConst lambda theta = (cost, grad)
+    where
+        m = fromIntegral (rows x)
+        h = sigmoid $ x <> theta
+        w = if useConst
+            then join [0, constant 1 (dim theta - 1)]
+            else constant 1 (dim theta)
+        theta' = w * theta
+        cost   = sumVector (y * log h + (1-y) * log (1-h)) / m + (sumVector $ mapVector (^2) theta') / (2 * m)
+        grad   = (1/m `scale` (y - h) <> x) + (lambda / m) `scale` theta'
 
 -- |Vectorized sigmoid function, sigmoid(x) = 1 / ( 1 + exp(-x) )
 sigmoid :: Vector Double -> Vector Double
